e0eb2d81539fa54eda27b269f6ad3a17fd02e31e
Durbaca Daniel Cristian <42060342+Stellrow@users.noreply.github.com>
2020-08-06 02:28:48 +0300

    Update MySQLInstance.java

--- MySQLHelper/src/ro/Stellrow/MySQLHelper/api/MySQLInstance.java
+++ MySQLHelper/src/ro/Stellrow/MySQLHelper/api/MySQLInstance.java
@@ -6,7 +6,9 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import java.sql.*;
+import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
+import java.util.function.BiConsumer;
 
 public class MySQLInstance{
     private String host;
@@ -57,10 +59,7 @@ public class MySQLInstance{
         });
 
     }
-    public ResultSet getData(String path){
-        return getData(path);
-    }
-    private CompletableFuture<ResultSet> getResult(String path){
+    public CompletableFuture<ResultSet> getResult(String path){
         return CompletableFuture.supplyAsync(() -> {
             try {
                 return statement.executeQuery(path);
